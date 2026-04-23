b8bf019782036420077cf38aed885fcdd7cdb0f9
Jules Clement Massonnat <julcleme@z1r3p1.42lyon.fr>
2026-04-23 10:49:58 +0200

    fixed

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -6,25 +6,24 @@ chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-
-    mysqld_safe --datadir=/var/lib/mysql &
-    
-    # Wait for server
-    until mysqladmin ping --silent; do
-        sleep 1
-    done
-
-    mysql <<EOF
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;
-EOF
-
-    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 fi
 
 sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-exec mysqld_safe --datadir=/var/lib/mysql
+mysqld_safe --datadir=/var/lib/mysql &
+
+# Wait for server
+until mysqladmin ping --silent; do
+    sleep 1
+done
+
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS \\`${MYSQL_DATABASE}\\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \\`${MYSQL_DATABASE}\\`.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+wait
