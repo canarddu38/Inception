0592b0e7ac2cfab3bd0ea57c437b61cec6efc4b0
Jules Clement Massonnat <julcleme@z1r3p1.42lyon.fr>
2026-04-23 10:22:21 +0200

    fixed

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -4,28 +4,27 @@ set -e
 mkdir -p /run/mysqld /var/lib/mysql
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
-if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null 2>&1
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
     mysqld_safe --datadir=/var/lib/mysql &
-    PID="$!"
     
-    until mysql -u root -e "SELECT 1" >/dev/null 2>&1; do
+    # Wait for server
+    until mysqladmin ping --silent; do
         sleep 1
     done
 
-    mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-    mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -u root -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
-    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+    mysql <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
 
     mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
-    wait "$PID"
 fi
 
-
-sed -i 's/bind-address\s*=.*$/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i 's/port\s*=.*$/port = 3306/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 exec mysqld_safe --datadir=/var/lib/mysql
